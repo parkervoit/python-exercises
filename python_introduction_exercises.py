7e11ef85903471d6e2e281f955c9ed7c7cbb6057
parkervoit <parker.r.voit@gmail.com>
2021-04-06 11:29:36 -0500

    added python intro exercises and jupyter notebook

---
+++ python_introduction_exercises.py
@@ -0,0 +1,10 @@
+#3. write a program that prints Hello, World!
+def hello():
+    return print('Hello, World!')
+
+hello()
+#4. write a variable called greeting that contains the message you will print to the console
+def hello():
+    greeting = print('Hello, World!')
+
+hello()
